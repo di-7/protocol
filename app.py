d9e29fbf79a908d4e50ef870736a73778829b877
di-7 <dukeindustries7@gmail.com>
2025-05-06 22:37:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def list_agents():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
